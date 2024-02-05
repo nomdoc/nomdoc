f0828266b6c3488cc0687e8cf05285059d7c62f6
Daniel Sam <9153498+sammkj@users.noreply.github.com>
2024-02-05 15:48:34 +0800

    Integrate prettier plugin for tailwind

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,5 +1,7 @@
 /** @type {import("prettier").Config} */
 module.exports = {
+  plugins: ["prettier-plugin-tailwindcss"],
+  tailwindFunctions: ["clsx", "cn"],
   trailingComma: "none",
   tabWidth: 2,
   semi: false,
