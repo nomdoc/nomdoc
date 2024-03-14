bbf678b0753bf2313aab335490408d6c724ee17d
Daniel Sam <9153498+sammkj@users.noreply.github.com>
2024-03-14 15:12:25 +0800

    Configure logger format

--- config/config.exs
+++ config/config.exs
@@ -107,7 +107,7 @@ config :tailwind,
 
 # Configures Elixir's Logger
 config :logger, :console,
-  format: "$time $metadata[$level] $message\n",
+  format: "$metadata[$level] $message\n",
   metadata: [:remote_ip, :request_id]
 
 # Use Jason for JSON parsing in Phoenix
