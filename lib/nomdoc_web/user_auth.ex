8a564f448c17cb1bba3ee08be749ade31b4a5e8e
Daniel Sam <9153498+sammkj@users.noreply.github.com>
2024-03-18 11:17:40 +0800

    Update log-in path

--- lib/nomdoc_web/user_auth.ex
+++ lib/nomdoc_web/user_auth.ex
@@ -160,7 +160,7 @@ defmodule NomdocWeb.UserAuth do
       socket =
         socket
         |> Phoenix.LiveView.put_flash(:error, "You must log in to access this page.")
-        |> Phoenix.LiveView.redirect(to: ~p"/users/log_in")
+        |> Phoenix.LiveView.redirect(to: ~p"/log-in")
 
       {:halt, socket}
     end
@@ -210,7 +210,7 @@ defmodule NomdocWeb.UserAuth do
       conn
       |> put_flash(:error, "You must log in to access this page.")
       |> maybe_store_return_to()
-      |> redirect(to: ~p"/users/log_in")
+      |> redirect(to: ~p"/log-in")
       |> halt()
     end
   end
